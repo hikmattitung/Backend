e9cce918bab096f7b08c911f942a7f8651e37c44
Hikmat Tamang <hikmattitung1111@gmail.com>
2026-04-14 18:09:02 +0545

    commit user

--- controller/user.js
+++ controller/user.js
@@ -1,6 +1,6 @@
 
 exports.userCreate = (req, res) => {
     res.status(200).json({
-        message: "hello form the backend Nepal! 
+        message: "hello form the backend Nepal! you're welcome here and wanna say something? I would like to hear from you!!!
     })
 }
